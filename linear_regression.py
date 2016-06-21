3b2ee01bbec08169fc107431ed46d79aa1fedf37
jeremyfromearth <jeremynealbrown@gmail.com>
2016-06-20 17:21:16 -0700

    Added an rss history to LinearRegressionModel, visualized in bear ipynb also added a simple example of the linear regression model

--- linear_regression.py
+++ linear_regression.py
@@ -7,10 +7,12 @@ class LinearRegressionModel:
         self.eta = 0.001
         self.tolerance = 0.01
         self.iterations = 0
+        self.rss = []
 
     def learn(self, data, features, target):
         # initialize member variables
         self.iterations = 0
+        self.residuals = []
         self.weights = np.zeros(1+len(features))
     
         # initialize gradient mag
@@ -26,7 +28,7 @@ class LinearRegressionModel:
             # accumulator for for gradient sum of squares
             gradient_sum_squares = 0.0
             # y-intercept
-            self.weights[0] -= np.sum(residuals) * self.eta
+            self.weights[0] -= self.eta * np.sum(residuals)
             
             # feature by feature update the weights
             for i in range(len(features)):
@@ -39,6 +41,8 @@ class LinearRegressionModel:
                 # set the new w-hat
                 self.weights[i+1] -= self.eta * partial
 
+            # store the residuals per each iteration
+            self.rss.append(np.sum(residuals ** 2))
             # measure the gradient
             gradient_magnitude = sqrt(gradient_sum_squares)
             # increment the iterations
@@ -46,25 +50,3 @@ class LinearRegressionModel:
 
     def predict(self, data, features):
         return np.dot(data[features], self.weights[1:]) + self.weights[0]
-
-if __name__ == '__main__':
-    import pandas as pd
-    features = ['x']
-    target = 'y'
-    data = pd.DataFrame(
-        data=list(zip([
-            1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0
-        ], 
-        [
-            4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 22.0
-        ])), columns=['x', 'y'])
-
-    model = LinearRegressionModel()
-    model.learn(data, features, target)
-    test = pd.DataFrame(data=list(zip([11.0], [24.0])), columns=['x', 'y'])
-    predictions = model.predict(test, features)
-    print('Predictions: {}, Iterations; {}, Weights: {}'.format(predictions, model.iterations, model.weights))
-    
-
-
-    
