cf9ed2b29817ef3b893e04308b867d6b01cbd8ef
jeremyfromearth <jeremynealbrown@gmail.com>
2016-07-28 16:09:07 -0700

    Now adding new article links to search space regardless of whether parent search met criteria, added code to remove duplicates from dataframe before saving (just in case)

--- mining/wikifauna.py
+++ mining/wikifauna.py
@@ -7,7 +7,7 @@ from zipfile import ZipFile
 
 # data vars
 count = 0
-limit = 10000
+limit = 100000
 new_articles = []
 article_titles_saved = set()
 article_titles_searched = set()
@@ -26,7 +26,7 @@ taxonomic_rank = [
 # open the existing data file and add existing titles to list of previously searched titles
 try: 
     df = pd.read_csv('./data/fauna.csv.gz', compression='gzip')
-    article_titles_searched.update(df['title'].tolist())
+    #article_titles_searched.update(df['title'].tolist())
     print('Opened existing data file with {} record(s)'.format(len(df)))
 except:
     df = pd.DataFrame(columns=['title', 'page-id', 'text'] + taxonomic_rank)
@@ -38,7 +38,7 @@ req = requests.get(api + '?action=query&list=embeddedin&eititle=Template:Species
 json = req.json()
 if 'query' in json and 'embeddedin' in json['query']:
     for obj in json['query']['embeddedin']:
-        if obj['title'] and obj['title'] not in article_titles_searched:
+        if obj['title'] and obj['title']:# not in article_titles_searched:
             article_search_space.add(obj['title'])
 print('Initial search space has {} records to mine'.format(len(article_search_space)))
 
@@ -66,9 +66,8 @@ def get_taxonomy_for_article(title):
 # saving anything that appears to be an animal species along the way
 while len(article_search_space) > 0 and count < limit:
     article_title = article_search_space.pop()
-    article_titles_searched.add(article_title)
     taxonomy = get_taxonomy_for_article(article_title)
-    print('Searching for {}'.format(article_title))
+    print('Crawlilng', article_title)
     if taxonomy is not None:
         # text
         url = api + '?format=json&action=query&prop=extracts&explaintext=&titles=' + article_title
@@ -84,43 +83,46 @@ while len(article_search_space) > 0 and count < limit:
             article_text = article_text.split('== References ==')[0]
             article_text = article_text.split('== Literature ==')[0]
 
-        # links
-        link_count = 0
-        if len(article_search_space) < limit:
-            url = api + '?action=query&prop=links&format=json&pllimit=500&titles=' + article_title
-            req = requests.get(url)
-            if req.status_code is not 200: continue
-            links = req.json()
-            if 'query' in links and 'pages' in links['query']:
-                node = links['query']['pages']
-                keys = node.keys()
-                for key in keys:
-                    if 'links' in node[key]:
-                        for link in node[key]['links']:
-                            if 'title' in link:
-                                new_title = link['title']
-                                if new_title not in article_titles_searched:
-                                    if 'Template' not in new_title:
-                                        link_count += 1
-                                        article_search_space.add(link['title'])
-
         # save the new article
-        print(article_title)
-        print('Adding {} new article titles'.format(link_count))
-        print('New search space has {} titles'.format(len(article_search_space)))
-        new_article = {'title' : article_title, 'page-id' : article_page_id, 'text' : article_text}
-        for rank in taxonomic_rank:
-            if rank in taxonomy:
-                new_article[rank] = taxonomy[rank]
-            else:
-                new_article[rank] = ''
+        if article_title not in article_titles_searched:
+            article_titles_searched.add(article_title)
+            new_article = {'title' : article_title, 'page-id' : article_page_id, 'text' : article_text}
+            for rank in taxonomic_rank:
+                if rank in taxonomy:
+                    new_article[rank] = taxonomy[rank]
+                else:
+                    new_article[rank] = ''
 
-        count += 1
-        new_articles.append(new_article)
-        article_titles_saved.add(article_title)
+            count += 1
+            new_articles.append(new_article)
+            article_titles_saved.add(article_title)
+            print('Adding', article_title, 'to corpus')
+            print('Corpus will contain', len(new_articles) + len(df))
+        
+    # links
+    link_count = 0
+    if len(article_search_space) < 1000:
+        url = api + '?action=query&prop=links&format=json&pllimit=500&titles=' + article_title
+        req = requests.get(url)
+        if req.status_code is not 200: continue
+        links = req.json()
+        if 'query' in links and 'pages' in links['query']:
+            node = links['query']['pages']
+            keys = node.keys()
+            for key in keys:
+                if 'links' in node[key]:
+                    for link in node[key]['links']:
+                        if 'title' in link:
+                            new_title = link['title']
+                            if new_title not in article_titles_searched:
+                                if 'Template' not in new_title:
+                                    link_count += 1
+                                    article_search_space.add(link['title'])
+                                    print('Adding article to search space', link['title'])
 
 # merge the new data with the 
 new_data = pd.DataFrame(new_articles)
 df = df.append(new_data)
+df.drop_duplicates()
 df.to_csv('./data/fauna.csv.gz', compression='gzip', index=False)
 print('Completed with {} records'.format(len(df)))
