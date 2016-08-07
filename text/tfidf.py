b79467f71f73938ab271c8bbeeb1a2a4e96f4e88
jeremyfromearth <jeremynealbrown@gmail.com>
2016-08-06 22:33:03 -0700

    Formalizing TFIDF

--- text/tfidf.py
+++ text/tfidf.py
@@ -1,4 +1,27 @@
 import pandas as pd
 
-df = pd.read_csv('./data/fauna.csv.gz', compression='gzip')
 
+def tfidf(corpus):
+    doc_freq = {}
+    term_freq = {}
+    
+    for index in corpus:
+        if index in term_freq:
+            terms = terms = term_freq[index]
+        else:
+            terms = term_freq[index] = {}
+    
+        s = str(corpus[index]).lower()
+        word_list = re.findall(r'\b[^\W\d_]+\b', s)
+
+        for word in word_list:
+            if word in terms:
+                terms[word] += 1
+            else:
+                terms[word] = 1
+
+            if word in doc_freq:
+                doc_freq[word].add(index)
+            else:
+                doc_freq[word] = set()
+                doc_freq[word].add(index)
