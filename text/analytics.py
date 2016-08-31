9c13d582f1eceb2ccca9bd3ae62b12f2f50863c8
jeremyfromearth <jeremynealbrown@gmail.com>
2016-08-31 12:19:12 -0700

    Finally able to store entire tfidf in a decent amount of time, now looking into sparse matrices to reduce memory consumption

--- text/analytics.py
+++ text/analytics.py
@@ -1,6 +1,8 @@
 import re
+import sys
 import numpy as np
 import pandas as pd
+from scipy.sparse import csr_matrix
 
 class TermFreqInverseDocFreq:
     def __init__(self):
@@ -13,36 +15,47 @@ class TermFreqInverseDocFreq:
         self.table = None
 
     def create(self, corpus, text_column, normalize = True):
-        term_freq = {}
-        unique_words = set()
         text = {}
-        for index, row in corpus.iterrows():
+        unique_words = set()
+        for page_id, row in corpus.iterrows():
             s = str(row[text_column]).lower()
             word_list = re.findall(r'\b[^\W\d_]+\b', s) 
-            text[index] = word_list
+            text[page_id] = word_list
 
             for word in word_list:
                 unique_words.add(word)
 
             self.word_count_over_iterations.append(len(unique_words))
 
-        print('unique_words created')
-
         self.unique_words = sorted(list(unique_words))
-        for index, row in corpus.iterrows():
-            terms = np.zeros(len(self.unique_words))
-            for word in text[index]:
-                terms[self.unique_words.index(word)] += 1
-            term_freq[index] = terms
-            print('term_freq created for document', index)
+        self.term_frequency = np.zeros(shape=(len(text), len(self.unique_words)), dtype=np.float)
+        reverse_word_lookup = dict({(self.unique_words[i], i) for i in range(len(self.unique_words))}) 
 
-        print(len(term_freq), len(term_freq[38082824]))
-        print(unique_words)
-        
+        n = 0
+        self.reverse_page_id_lookup = {}
+        for page_id, words in text.items():
+            #terms = np.zeros(len(self.unique_words))
+            self.reverse_page_id_lookup[page_id] = n
+            for word in words:
+                self.term_frequency[n][reverse_word_lookup[word]] += 1
+            print('Page', page_id, '-', n, 'processed')
+            n += 1
+       
+        sparse = csr_matrix(self.term_frequency, dtype=np.float)
+        print(sys.getsizeof(self.term_frequency))
+        print(sys.getsizeof(sparse))
+
+        page_id = 3451749
+        page_index = self.reverse_page_id_lookup[page_id]
+        word = 'foothill'
+        word_index = reverse_word_lookup[word]
+        print(sparse[page_index, word_index])
+
         '''
-        print('Term frequency dict created')
-        self.document_ids = list(term_freq.keys())
-
+        if normalize:
+            np.apply_along_axis(lambda x : x / np.sqrt(np.sum(np.power(x, 2))), 1, self.term_frequency)
+            print('Term frequency normalized')
+        
         tf = []
         self.all_words = sorted(list(unique_words))
         for id in self.document_ids:
@@ -61,9 +74,7 @@ class TermFreqInverseDocFreq:
         self.term_frequency = self.term_frequency.fillna(0)               
         print('Term frequency na filled')
         # normalize each row so that it's magnitude == 1.0
-        if normalize:
-            self.term_frequency = self.term_frequency.apply(lambda x : x / np.sqrt(np.sum(np.power(x.values, 2)))) 
-            print('Term frequency normalized')
+        
         # conver the term frequency to a Series representing document frequency
         self.document_frequency = self.term_frequency.astype(bool).sum(axis=1)
         print('Document frequency DataFrame created')
