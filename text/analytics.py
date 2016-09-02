b8cc385e43af126170f2df4e86e6339a638101ca
jeremyfromearth <jeremynealbrown@gmail.com>
2016-09-02 15:30:06 -0700

    Cleaned up code and made variable names more consistent, added interface for fetching values from matrices within the tfidf, updated examples

--- text/analytics.py
+++ text/analytics.py
@@ -6,61 +6,92 @@ from scipy.sparse import csr_matrix, csc_matrix
 
 class TermFreqInverseDocFreq:
     def __init__(self):
-        self.terms = {}
+        self.terms = None
         self.term_frequency = None
+        self.doc_id_to_index = None
         self.document_frequency = None
         self.inverse_document_frequency = None
-        self.word_count_over_iterations = []
-        self.table = None
+        self.term_count_over_iterations = None
 
     def create(self, corpus, text_column, normalize = True):
-        print('TermFreqInverseDocFreq.create()')
         data = []
         indptr = [0]
         indices = []
         self.terms = {}
-        page_id_to_index = {}
-        for page_id, row in corpus.iterrows():
+        self.doc_id_to_index = {}
+        self.term_count_over_iterations = []
+        for doc_id, row in corpus.iterrows():
             text = str(row[text_column]).lower()
             terms = re.findall(r'\b[^\W\d_]+\b', text)
-            page_id_to_index.setdefault(page_id, len(page_id_to_index))
-            for word in terms:
-                index = self.terms.setdefault(word, len(self.terms))
+            for term in terms:
+                index = self.terms.setdefault(term, len(self.terms))
                 indices.append(index)
                 data.append(1)
             indptr.append(len(indices))
-            self.word_count_over_iterations.append(len(self.terms))
+            self.term_count_over_iterations.append(len(self.terms))
+            self.doc_id_to_index.setdefault(doc_id, len(self.doc_id_to_index))
         
         # create a sparse matrix representation of the term frequencies
         #
-        # page-id, a, as, be, both
-        # 2342342, 2,  3,  5,    1
-        # 0948333, 2,  3,  5,    1
+        # doc-id,  a,  as, be, bovid
+        # 2342342, 2,  3,  5,  4
+        # 0948333, 2,  3,  5,  0
         # 
         self.term_frequency = csr_matrix((data, indices, indptr), dtype=np.float)
-        self.term_frequency.sum_duplicates()
-        print('Term frequency created')
 
+        # the data format above will create an entry for every instance of a term
+        # so, there could be multiple values for (0, 0)
+        # summing the duplicates enables us to remove the duplicate entries 
+        # later we can use this matrix to derive the document frequency matrix
+        self.term_frequency.sum_duplicates()
+
+        # normalize this if the flag is set
+        # each row in the resulting matrix will now have a magnitude of 1.0
+        # this is an especially important step if there is a lot of disparity the length of the documents
+        #
         if normalize:
             tf = self.term_frequency 
             self.term_frequency = tf.multiply(csr_matrix(1/np.sqrt(tf.multiply(tf).sum(1))))
-            print('Term frequency normalized')
 
         # create a document frequency representation
+        # how many documents does each term show up in
+        #
+        # a,        as,    be,     bovid 
+        # 23123123, 23425, 235235, 2 
+        #
         self.document_frequency = self.term_frequency.astype(np.bool).sum(0)
-
+        
+        # create the inverse document frequency
+        # 
+        # doc-id, a,     as,     be,   bovid
+        # 5234,   0.234, 0.0123, 0.01, 8.345
         self.inverse_document_frequency = csr_matrix(np.apply_along_axis(\
             lambda x : np.log(self.term_frequency.shape[0] / (1 + x)), 1, self.document_frequency))
-        self.tfidf = self.term_frequency.multiply(self.inverse_document_frequency)
-        print('TFIDF created')
 
-        word = 'antelope'
-        page_id = 1369072
-        page_index = page_id_to_index[page_id]
-        word_index = self.terms[word]
-        print('Page Index', page_index, 'Word Index', word_index)
-        print('Page:', page_id, 'Term:', word) 
-        print('Global Rarity:', self.inverse_document_frequency[0, word_index])
-        print('Local frequency', self.term_frequency[page_index, word_index])
-        print('Gloabl frequency:', self.document_frequency[0, word_index])
-        print('TFIDF', self.tfidf[page_index, word_index])
+        # create the tfidf
+        self.tfidf = self.term_frequency.multiply(self.inverse_document_frequency)
+
+
+    # get a single row from the tfidf
+    def __getitem__(self, document_id):
+        return self.tfidf[self.doc_id_to_index[document_id]]
+
+    # get the document frequency for a single term
+    def get_doc_freq(self, term):
+        term_index = self.terms[term]
+        return self.document_frequency[0, term_index]
+
+    # get the local frequency for a single term 
+    # if not normalized returns an integer indicating the number of times the term is in the supplied document
+    # if normalized returns a floating point indicating the component 
+    # value of a unit vector representing all the terms in the supplied document
+    def get_local_freq(self, document_id, term):
+        doc_index = self.doc_id_to_index[document_id]
+        term_index = self.terms[term]
+        return self.term_frequency[doc_index, term_index]
+
+    # returns a score indicating the term's rarity within the corpus
+    # the higher the value, the more rare the term
+    def get_global_rarity(self, term):
+        term_index = self.terms[term]
+        return self.inverse_document_frequency[0, term_index]
--- text/tfidf_tests.py
+++ text/tfidf_tests.py
@@ -1,9 +1,22 @@
+import sys
 import pandas as pd
 from analytics import TermFreqInverseDocFreq
 
 df = pd.read_csv('./data/fauna.csv.gz', compression='gzip')
 df = df.set_index(keys=['page-id'])
-tfidf = TermFreqInverseDocFreq()
-tfidf.create(df[:3000], 'text', True)
-print('complete')
 
+tfidf = TermFreqInverseDocFreq()
+tfidf.create(df, 'text', True)
+
+term = 'antelope'
+doc_id = 1369072
+doc_index = tfidf.doc_id_to_index[doc_id]
+term_index = tfidf.terms[term]
+
+print('Document Id:', doc_id, 'Term:', term) 
+print('Document Index', doc_index, 'Term Index', term_index)
+print('Local Frequency', tfidf.get_local_freq(doc_id, term))
+print('Global Frequency: ', tfidf.get_doc_freq(term))
+print('Global Rarity Score:', tfidf.get_global_rarity(term))
+print('TFIDF Score for document', tfidf[doc_id][0, term_index])
+print('TFIDF.shape', tfidf[doc_id].shape)
