cb8c81ad306c1f8cb019104d42ca214af03ab117
jeremyfromearth <jeremynealbrown@gmail.com>
2016-09-07 00:08:16 -0700

    Added better comments to LinearRegressionModel

--- linear-regression/linear_regression.py
+++ linear-regression/linear_regression.py
@@ -11,15 +11,28 @@ class LinearRegressionModel:
         self.gradient_magnitudes = []
 
     def learn(self, data, features, target):
+        '''
+        Trains the model 
+
+        Parameters
+        ---------
+        data : pd.DataFrame
+            N-1Dimensional array of data
+        features : list
+            A list of strings indicating which columns represent features
+        target : string
+            The name of the column to predict
+        '''
         # initialize member variables
         self.iterations = 0
         self.residuals = []
         self.weights = np.zeros(1+len(features))
     
         # initialize gradient mag
+        # initialize a list to store magnitudes over iterations
         # this is a measure of the magnitude of the gradient of the aproximated function
-        gradient_magnitude = float('inf')
         self.gradient_magnitudes = []
+        gradient_magnitude = float('inf')
 
         # gradient descent
         while gradient_magnitude > self.tolerance:
@@ -52,9 +65,31 @@ class LinearRegressionModel:
             self.iterations += 1
 
     def predict(self, data, features):
+        '''
+        Returns a list of predictions for the supplied data
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            A dataframe containg data to make predictions on
+        features : list
+            A list of columns names to base predictions on
+        '''
         return np.dot(data[features], self.weights[1:]) + self.weights[0]
 
     def get_squared_error_loss(self, data, features, target):
+        '''
+        Returns the squared error loss for the supplied data
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Data to assess squared error on
+        features : list
+            A list of column names to base assessment on
+        target : string
+            Name of the column to predict
+        '''
         predictions = self.predict(data, features)
         errors = data[target] - predictions
         return (1.0 / len(data)) * np.sum(errors **2)
