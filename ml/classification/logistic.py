3da9ae81e412bf190b19403201fc93dce72624db
jeremyfromearth <jeremynealbrown@gmail.com>
2017-03-23 22:31:37 -0700

    Implemented first version of logistic regression

--- ml/classification/logistic.py
+++ ml/classification/logistic.py
@@ -1,18 +1,27 @@
+from ml.math.kernels import sigmoid
+from ml.optimization.gradient_descent import SSEGradientDescent
+from ml.optimization.regularization import L2Regularization
+
 class LogisticRegressionModel:
     def __init__(self):
         self.params = []
-        self.optimization = GradientDescentOptmizer()
+        self.optimization = SSEGradientDescent()
         self.regularization = L2Regularization()
 
     def learn(self, x, y):
-        # train the model using the specified optimization
-        pass
-
+        m = x.shape[0]
+        n = x.shape[1] + 1
+        self.params = np.zeros(n)
+        X = np.concatenate((np.ones([m, 1]), x), axis=1)
+        while not self.optimization.converged:
+            p_update = self.optimization.step(X, self.predict(X)[1], y)
+            #r_update = self.regularization.step(m, self.optimization.η)
+            self.params[0] = self.params[0] - p_update[0]
+            #self.params[1:] = self.params[1:] * r_update - p_update[1:]
+            self.params[1:] -= p_update[1:]
+            
     def predict(self, x):
-        # make a class prediction from the input
-        pass
-
-    def compute_cost(self):
-        # not necessary for actually training the model, but useful for testing and validation
-        pass
-
+        prob = sigmoid(x * self.params)
+        return prob, round(prob)
+    
+    
