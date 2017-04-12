Fixed multiple issue with gradient descent and regularization classes, cleaned up logistic.py and updated/renamed logistic regression example

ml/classification/logistic.py
@@ -10,22 +10,24 @@ class LogisticRegressionModel:
         self.regularization = L2Regularization()
         self.cost_over_time = []
         self.params_over_time = []
+        self.max_iterations = 5000
 
     def learn(self, x, y):
+        iters = 0
         m = x.shape[0]
         n = x.shape[1] + 1
         self.cost_over_time = []
         self.params = np.zeros(n)
         self.params_over_time = []
         X = np.concatenate((np.ones([m, 1]), x), axis=1)
-        while not self.optimization.converged:
+        while not self.optimization.converged and iters < self.max_iterations:
             prediction = self.predict(X)
             self.cost_over_time.append(self.compute_cost(prediction[0], y))
-            p_update = self.optimization.step(X, prediction[0], y)
-            r_update = self.regularization.step(m, self.optimization.learning_rate)
+            p_update = self.optimization.step(X, prediction[0], y, self.regularization, self.params)
             self.params[0] = self.params[0] - p_update[0]
-            self.params[1:] = self.params[1:] * r_update - p_update[1:]
+            self.params[1:] = self.params[1:] - p_update[1:]
             self.params_over_time.append(list(self.params))
+            iters += 1
             
     def predict(self, x):
         z = np.dot(x, self.params)
@@ -34,5 +36,6 @@ class LogisticRegressionModel:
     
     def compute_cost(self, prediction, y):
         element_wise_cost = y * np.log(prediction) + (1 - y) * np.log(1-prediction)
-        cost = -(1.0 / y.shape[0]) * np.sum(element_wise_cost)
+        regularization_term = self.regularization.cost(y.shape[0], self.params[1:])
+        cost = -(1.0 / y.shape[0]) * np.sum(element_wise_cost) + regularization_term
         return cost

ml/optimization/gradient_descent.py
@@ -10,10 +10,24 @@ class SSEGradientDescent:
         self.mag = []
         self.tolerance = 0.0000001
         
-    def step(self, x, p, y):
+    def step(self, x, p, y, r, params):
+        '''
+            x - features
+            p - predictions
+            y - actual outputs
+            r - regurlarization
+            params - model params
+        '''
+        
         error = p - y
         delta = np.dot(error, x)
+        regularization = r.derivative(x.shape[0], params[1:])
+        gamma = (1 / x.shape[0]) * delta
+        gamma[1:] = gamma[1:] + regularization
+        new_params = self.learning_rate *  gamma
+        
         mag = sum(delta) ** 2
         self.mag.append(mag)
         self.converged = mag < self.tolerance
-        return (self.learning_rate / x.shape[0]) * delta
+        
+        return new_params