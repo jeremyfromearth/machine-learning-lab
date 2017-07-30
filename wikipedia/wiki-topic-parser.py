7b9b151dc822e59e9786dc1f4cb3a56adb5639e9
jeremyfromearth <jeremynealbrown@gmail.com>
2017-07-30 11:42:17 -0700

    Fixed bug where title case article titles returned empty results, removed splitting of extract, extract and url props to resultant json

--- wikipedia/wiki-topic-parser.py
+++ wikipedia/wiki-topic-parser.py
@@ -9,11 +9,12 @@ Downloads an article from the wikipedia api and splits it into sections
 """
 
 if len(sys.argv) > 1:
-    articles = []
-    title = sys.argv[1]
+    title = sys.argv[1].lower()
     base = "https://en.wikipedia.org/"
     api = "w/api.php?format=json&action=query&prop=extracts&explaintext=&titles={}"
-    data = requests.get(base + api.format(title)).json()
+
+    article = {'extract': '', 'url': base + api.format(title)}
+    data = requests.get(article['url']).json()
 
     if "query" in data:
         q = data["query"]
@@ -23,16 +24,18 @@ if len(sys.argv) > 1:
             page_id = list(p.keys())[0]
             if page_id in p:
                 if "extract" in p[page_id]:
-                    extract = p[page_id]["extract"]
+                    article['extract'] = p[page_id]["extract"]
+                    '''
                     sections = re.split(r'=+', extract)
                     for section in sections:
                         if len(section) > 300:
-                            articles.append(section)
+                            article["extract"] = section
+                    '''
 
     dirname = os.path.dirname(__file__)
-    filename = "-".join(title.strip().lower().split(' '))
+    filename = "-".join(title.strip().lower().split(' ')) + '.json'
     filepath = os.path.join(dirname, filename)
     with open(filepath, 'w') as f:
-        f.write(json.dumps(articles))
+        f.write(json.dumps(article))
 else:
     print("Please supply an article title")
