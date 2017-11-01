5321ea992b7a04e2f4ecd4fcecd48dec9edd47e4
jeremyfromearth <jeremynealbrown@gmail.com>
2017-10-31 17:52:46 -0700

    Made a little more progress on the deep net model

--- ml/neural_network.py
+++ ml/neural_network.py
@@ -1,7 +1,11 @@
 import numpy as np
+try:
+  from ml.kernels import sigmoid, relu
+except:
+  from kernels import sigmoid, relu
 
 class NeuralNetwork:
-    def __init__(self, layers, rseed=0):
+    def __init__(self, layers, rseed=0, activation=sigmoid):
       np.random.seed(rseed)
       self.e = 0.1
       self.a = {}
@@ -11,7 +15,6 @@ class NeuralNetwork:
 
       # append an extra layer here
       # will need to change this in the event that we are doing multi-class classification
-      layers = layers + [1]
       self.nl = len(layers)
       for i in range(0, self.nl-1):
         self.b[i] = np.zeros((layers[i+1], 1))
@@ -27,21 +30,28 @@ class NeuralNetwork:
         s += 'Layer: ' + str(k) + ' , Shape: ' + str(v.shape) + '\n' +  str(v) + '\n'
       return s
 
+    def forward(self, X):
+      # TODO: Implement mini batch
+      self.z = {}
+      self.a[0] = np.copy(X)
+      for i in range(1, self.nl):
+        self.z[i] = np.dot(self.w[i-1], self.a[i-1])
+        self.a[i] = relu(self.z[i])
 
-    def forward(self, A0):
-      for i in range(0, self.nl):
-        pass
-
-    def back(self, A0):
+    def backward(self, Y):
       pass
 
-    def train(self, X):
+    def learn(self, X, Y, iters=1000):
+      for i in range(0, iters):
+        self.forward(X)
+        self.backward(Y)
       pass
 
 if __name__ == '__main__':
-    nn = NeuralNetwork(layers=[8, 6, 4], rseed=32)
+    nn = NeuralNetwork(layers=[2, 6, 4, 1], rseed=32)
     a = np.random.rand(3, 100)
     w = np.random.rand(8, 3)
     b = np.zeros([8, 1])
     z = np.dot(w, a)  + b
-    print(z.shape)
+    print(nn)
+
